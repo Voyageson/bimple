79f8b1ddfa8b45501d2bc4aa2186b9491b7dc604
Voyageson <2582376763@qq.com>
2024-04-01 01:08:08 +0800

    1. fix: multi-page option is hidden 2. style: delete some useless js file

--- content/index.js
+++ content/index.js
@@ -1,19 +1,14 @@
+// remove search placeholder
+const targetNode = document.getElementsByClassName("nav-search-input")[0]
 
-// 
-const targetNode = document.getElementsByClassName('nav-search-input')[0];  // 选择需要观察变动的节点
-const config = { attributes: true, attributeFilter: ['placeholder', 'title'] };  // 观察器的配置（需要观察什么变动）
-const callback = function(mutationsList, observer) {
-	// Use traditional 'for loops' for IE 11
-	for(let mutation of mutationsList) {
-        if (mutation.type === 'attributes') {
-			targetNode.setAttribute('placeholder', '');
-			targetNode.setAttribute('title', '');
-            observer.disconnect();
-		}
-	}
-};
+const observer = new MutationObserver((mutationList, observer) => {
+  console.log("mutate")
+  for (const mutation of mutationList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "placeholder") {
+      observer.disconnect();
+      targetNode.setAttribute("placeholder", "")
+    }
+  }
+});
 
-// 创建一个观察器实例并传入回调函数
-const observer = new MutationObserver(callback);
-// 以上述配置开始观察目标节点
-observer.observe(targetNode, config);
+observer.observe(targetNode, { attributes: true, childList: false, subtree: true });
